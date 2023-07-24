ba8be1826304498086b82b85bec7cc79b28e74b6
Foxsiny <mitiashkinata8@gmail.com>
2023-07-24 16:52:35 +0300

    feature

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 
 
-
+use App\Http\Controllers\AccountController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\IndexController as AdminController;
 use App\Http\Controllers\NewsController;
@@ -29,18 +30,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Route::get('/hello/{name}', function (string $name) {
-//    return "Hello, $name";
-//});
+Route::group(['middleware' => 'auth'], static function () {
+    Route::group(['prefix' => 'account'], static function () {
+        Route::get('/', AccountController::class)->name('account');
+    });
 
 // Admin
-
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], static function() {
-    Route::get('/', AdminController::class)
-        ->name('index');
-    Route::resource('/categories', AdminCategoryController::class);
-    Route::resource('/news', AdminNewsController::class);
+    Route::group([
+        'prefix' => 'admin',
+        'as' => 'admin.',
+        'middleware' => 'check.admin',
+    ], static function () {
+        Route::get('/', AdminController::class)
+            ->name('index');
+        Route::resource('/categories', AdminCategoryController::class);
+        Route::resource('/news', AdminNewsController::class);
+    });
 });
+
+
 
 
 
@@ -67,3 +75,19 @@ Route::get('/collections', function () {
     $collection = collect([1,2,3,4,5,77,8,9,34,56,86,64]);
     dd($collection->toJson());
 });
+
+Route::get('/sessions', function () {
+    if (session()->has('mysession')) {
+        //dd(session()->all(), session()->get('mysession'));
+        session()->forget('mysession');
+
+        //session(['mysession' => 'Data']);
+    }
+
+    //session()->put('mysession', 'Test Session');
+});
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
